f00ae56a0a39acab66acee3f059c61861d06f7d5
Samuel Jalade <32564906+JaladeSamuel@users.noreply.github.com>
2017-11-22 22:04:33 +0100

    published python

---
+++ pub.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+import time
+
+PORT = 2532
+client = mqtt.Client("sampublie")
+client.connect("91.224.148.106",PORT)
+i = 0
+debut=time.time()
+while(i=<500):
+	client.publish("de-sam","test du broker "+str(i))
+	i+=1
+	time.sleep(0.01)
+fin = time.time()
+temp = fin - debut
+client.publish("de-sam","TEMPS ECOULE : "+str(temp))
+print(str(temp))
