2f9034fc8883c35dd0489cc7478d6d6d3f0e3a16
Samuel Jalade <32564906+JaladeSamuel@users.noreply.github.com>
2017-11-22 22:08:35 +0100

    Add files via upload

--- pub.py
+++ pub.py
@@ -6,8 +6,9 @@ client = mqtt.Client("sampublie")
 client.connect("91.224.148.106",PORT)
 i = 0
 debut=time.time()
-while(i=<500):
+while(i<=500):
 	client.publish("de-sam","test du broker "+str(i))
+	#client.publish("de-sam","")
 	i+=1
 	time.sleep(0.01)
 fin = time.time()
