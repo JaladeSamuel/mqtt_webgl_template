6acd0bca6d291b143d7a714e5d2a06bbf5ba73c7
Kilian Desportes <kilian.desportes@outlook.com>
2018-01-08 16:18:05 +0100

    actualisation template obj

--- templateObject/js/templateObj.js
+++ templateObject/js/templateObj.js
@@ -7,89 +7,90 @@ var player = { height:1.8, speed:0.2, turnSpeed:Math.PI*0.02 };
 var USE_WIREFRAME = false;
 
 
-  ratio = window.innerWidth / window.innerHeight;
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(90, ratio, 0.1, 1000);
-  light = new THREE.PointLight(0xffffff, 0.8, 18);
-  light.position.set(-3,6,-3);
+	ratio = window.innerWidth / window.innerHeight;
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(90, ratio, 0.1, 1000);
 
-  scene.add(light);
+	light = new THREE.PointLight(0xffffff, 0.8, 18);
+	light.position.set(-3,6,-3);
 
-    // Model/material loading!
-  var mtlLoader = new THREE.MTLLoader();
-  mtlLoader.load("Objets/MTL/Tent_Poles_01.mtl", function(materials){
-  materials.preload();
-  var objLoader = new THREE.OBJLoader();
-  objLoader.setMaterials(materials);
+	scene.add(light);
 
-  objLoader.load("Objets/OBJ/test.obj", function(mesh){
+		// Model/material loading!
+	var mtlLoader = new THREE.MTLLoader();
+	mtlLoader.load("Objets/MTL/Tent_Poles_01.mtl", function(materials){
+	materials.preload();
+	var objLoader = new THREE.OBJLoader();
+	objLoader.setMaterials(materials);
 
-    mesh.traverse(function(node){
-      if( node instanceof THREE.Mesh ){
-        node.castShadow = true;
-        node.receiveShadow = true;
-      }
-    });
+	objLoader.load("Objets/OBJ/Puss_in_Boots.obj", function(mesh){
 
-    scene.add(mesh);
-    mesh.position.set(-5, 0, 4);
-    mesh.rotation.y = -Math.PI/4;
-    });
-  });
+		mesh.traverse(function(node){
+			if( node instanceof THREE.Mesh ){
+				node.castShadow = true;
+				node.receiveShadow = true;
+			}
+		});
+
+		scene.add(mesh);
+		mesh.position.set(-5, 0, 4);
+		mesh.rotation.y = -Math.PI/4;
+		});
+	});
 
 
-  camera.position.set(0, player.height, -5);
-  camera.lookAt(new THREE.Vector3(0,player.height,0));
+	camera.position.set(0, player.height, -5);
+	camera.lookAt(new THREE.Vector3(0,player.height,0));
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
 
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.BasicShadowMap;
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.BasicShadowMap;
 
-  document.body.appendChild(renderer.domElement);
+	document.body.appendChild(renderer.domElement);
 
-  animate();
+	animate();
 
 function animate(){
-  requestAnimationFrame(animate);
+	requestAnimationFrame(animate);
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
 
-  if(keyboard[90]){ // Z -> avant
-    camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-    camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
-  }
-  if(keyboard[83]){ // S -> arriere
-    camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-    camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
-  }
-  if(keyboard[81]){ // Q -> gauche
-    camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
-    camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
-  }
-  if(keyboard[68]){ // D -> droite
-    camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
-    camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
-  }
+	if(keyboard[90]){ // Z -> avant
+		camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[83]){ // S -> arriere
+		camera.position.x += Math.sin(camera.rotation.y) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
+	}
+	if(keyboard[81]){ // Q -> gauche
+		camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
+	}
+	if(keyboard[68]){ // D -> droite
+		camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
+		camera.position.z += -Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
+	}
 
-  if(keyboard[37]){ // left arrow key
-    camera.rotation.y -= player.turnSpeed;
-  }
-  if(keyboard[39]){ // right arrow key
-    camera.rotation.y += player.turnSpeed;
-  }
+	if(keyboard[37]){ // left arrow key
+		camera.rotation.y -= player.turnSpeed;
+	}
+	if(keyboard[39]){ // right arrow key
+		camera.rotation.y += player.turnSpeed;
+	}
 
-  renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 function keyDown(event){
-  keyboard[event.keyCode] = true;
+	keyboard[event.keyCode] = true;
 }
 
 function keyUp(event){
-  keyboard[event.keyCode] = false;
+	keyboard[event.keyCode] = false;
 }
 
 window.addEventListener('keydown', keyDown);
