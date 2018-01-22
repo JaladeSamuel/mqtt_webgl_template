fbd3e33aff4f364b0af8b3ec62ae845bf846456c
desportes <kilian.desportes@outlook.com>
2018-01-22 21:30:16 +0100

    args pour le python

--- PythonMQTT/text.py
+++ PythonMQTT/text.py
@@ -1,21 +1,20 @@
 import paho.mqtt.client as mqtt
-import json
+import json, sys
 import time
+
+arg = sys.argv[1]
 
 MQTT_PORT = 2532
 MQTT_TOPIC = "JSONtemplate"
 MQTT_IP = "91.224.148.106"
 
-file = open("horse.js","r")
+file = open(arg,"r")
 client = mqtt.Client("Kilian")
 client.connect(MQTT_IP,MQTT_PORT)
-debut=time.time()
-tab=[]
-while len(file.readline())>0:
-	tab.append(file.readline())
+tab=file.readlines()
+print(tab)
 client.publish(MQTT_TOPIC,len(tab))
 for i in range(len(tab)):
+	time.sleep(0.1)
 	client.publish(MQTT_TOPIC,tab[i])
-fin = time.time()
-print(fin-debut)
 file.close()
