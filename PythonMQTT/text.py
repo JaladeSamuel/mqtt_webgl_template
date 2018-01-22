cb9eca8840af4fc4388fb0d921108586f711bf9e
desportes <kilian.desportes@outlook.com>
2018-01-22 17:55:36 +0100

    soft python mqtt json

--- PythonMQTT/text.py
+++ PythonMQTT/text.py
@@ -3,16 +3,19 @@ import json
 import time
 
 MQTT_PORT = 2532
-MQTT_TOPIC = "testMQTT"
+MQTT_TOPIC = "JSONtemplate"
 MQTT_IP = "91.224.148.106"
 
 file = open("horse.js","r")
 client = mqtt.Client("Kilian")
 client.connect(MQTT_IP,MQTT_PORT)
 debut=time.time()
-while len(file.read(1))>0:
-	char = file.read(1)
-	client.publish(MQTT_TOPIC,file.read(1))
+tab=[]
+while len(file.readline())>0:
+	tab.append(file.readline())
+client.publish(MQTT_TOPIC,len(tab))
+for i in range(len(tab)):
+	client.publish(MQTT_TOPIC,tab[i])
 fin = time.time()
 print(fin-debut)
 file.close()
