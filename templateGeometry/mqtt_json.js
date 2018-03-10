35f9b51470495837272da847df6e431a49ea2353
kilian desportes <kilian.desportes@outlook.com>
2018-03-10 12:00:44 +0100

    modif mqtt topic avec id, fini

--- templateGeometry/mqtt_json.js
+++ templateGeometry/mqtt_json.js
@@ -19,11 +19,10 @@ client.onMessageArrived = function (message) {
 			console.log("Message : "+msg);
 			var splitTopic=destination.split("/");
 			console.log(splitTopic)
+
 			if(splitTopic[1]=="retourID"){
-				console.log("_____________________________");
 				console.log("Arrivé d'un ID apres création");
 				console.log("ID :"+msg);
-				console.log("_____________________________");
 			}else if(splitTopic[1]=="scene"){
 				console.log("Scene");
 				console.log("Message : "+msg);
@@ -37,83 +36,79 @@ client.onMessageArrived = function (message) {
 					case"rotate":
 						rotateScene(tab[1]);
 						break;
-				console.log("_____________________________");
+					}
 			}else{
 				switch(splitTopic[2]){
-				case "templateGeo/retourID/":
-					console.log("_____________________________");
-					console.log("Arrivé d'un ID apres création");
-					console.log("ID :"+msg);
-					console.log("_____________________________");
-					break;
-				case "templateGeo/obj/create/":
-					console.log("Create");
-					console.log("Objet : "+msg);
-					console.log("Compteur d'object : " +cpt);
-					switch(msg){
-						case "Cube" :
-							AjoutObjectGeometry("BoxGeometry");
-							break;
-						case "Sphere" :
-							AjoutObjectGeometry("SphereGeometry");
-							break;
-						case "Plan" :
-							AjoutObjectGeometry("PlaneGeometry");
-							break;
-						case "Point" :
-							AjoutObjectGeometry("PointGeometry");
-							break;
-					}
-
-					client.subscribe("templateGeo/obj/delete/"+cpt);
-					client.subscribe("templateGeo/obj/position/"+cpt);
-					client.subscribe("templateGeo/obj/scale/"+cpt);
-					client.subscribe("templateGeo/obj/color/"+cpt);
-					client.subscribe("templateGeo/obj/select/"+cpt);
-					//Retour de l'ID
-					console.log("PUSH ID on TOPIC : templateGeo/retourID/");
-					msage = new Paho.MQTT.Message(''+cpt);
-					msage.destinationName = "templateGeo/ID/";
-					client.send(msage);
-					cpt = cpt + 1;
-					console.log("_____________________________");
-					break;
-				case "templateGeo/obj/delete/":
-					console.log("Delete");
-					console.log("ID : "+msg);
-					deleteElement(msg);
-					console.log("_____________________________");
-					break;
-				case "templateGeo/obj/position/":
-					var tab=msg.split("/");
-					console.log("Position");
-					console.log("ID : "+tab[0]);
-					console.log("Positions :"+tab[1]);
-					var vpos=tab[1].split(",");
-					var vx = vpos[0];
-					var vy = vpos[1];
-					var vz = vpos[2];
-					setPosition(tab[0],vx,vy,vz);
-					console.log("_____________________________");
-					break;
-				case "templateGeo/obj/scale/":
-					var tab=msg.split("/");
-					console.log("Scale");
-					console.log("ID : "+tab[0]);
-					console.log("Echelle :"+tab[1]);
-					setScale(tab[0],tab[1]);
-					break;
-				case "templateGeo/obj/color/":
-					var tab=msg.split("/");
-					console.log("Color");
-					console.log("ID : "+tab[0]);
-					console.log("Color :"+tab[1]);
-					setColor(tab[0],tab[1]);
-					break;
-				case "templateGeo/obj/select/":
-					console.log("Select");
-					console.log("ID : "+msg);
-					selectObject(msg,lastSelect);
-					break;
+					case "create":
+						var idObj=splitTopic[3];
+						console.log("Create");
+						console.log("Objet : "+msg);
+						console.log("ID de l'objet : " +cpt);
+						switch(msg){
+							case "Cube" :
+								AjoutObjectGeometry("BoxGeometry");
+								break;
+							case "Sphere" :
+								AjoutObjectGeometry("SphereGeometry");
+								break;
+							case "Plan" :
+								AjoutObjectGeometry("PlaneGeometry");
+								break;
+							case "Point" :
+								AjoutObjectGeometry("PointGeometry");
+								break;
+						}
+						client.subscribe("templateGeo/obj/delete/"+cpt+"/");
+						console.log("subscribe templateGeo/obj/delete/"+cpt+"/")
+						client.subscribe("templateGeo/obj/position/"+cpt+"/");
+						client.subscribe("templateGeo/obj/scale/"+cpt+"/");
+						client.subscribe("templateGeo/obj/color/"+cpt+"/");
+						client.subscribe("templateGeo/obj/select/"+cpt+"/");
+						//Retour de l'ID
+						console.log("PUSH ID on TOPIC : templateGeo/retourID/");
+						msage = new Paho.MQTT.Message(''+cpt);
+						msage.destinationName = "templateGeo/ID/";
+						client.send(msage);
+						cpt = cpt + 1;
+						break;
+					case "delete":
+						var idObj=splitTopic[3];
+						console.log("Delete");
+						console.log("ID : "+idObj);
+						deleteElement(idObj);
+						break;
+					case "position":
+						var idObj=splitTopic[3];
+						console.log("Position");
+						console.log("ID : "+idObj);
+						console.log("Positions :"+msg);
+						var vpos=msg.split(",");
+						console.log("pos"+vpos)
+						var vx = vpos[0];
+						var vy = vpos[1];
+						var vz = vpos[2];
+						setPosition(idObj,vx,vy,vz);
+						break;
+					case "scale":
+					  var idObj=splitTopic[3];
+						console.log("Scale");
+						console.log("ID : "+idObj);
+						console.log("Echelle :"+msg);
+						setScale(idObj,msg);
+						break;
+					case "color":
+						var idObj=splitTopic[3];
+						console.log("Color");
+						console.log("ID : "+idObj);
+						console.log("Color :"+msg);
+						setColor(idObj,msg);
+						break;
+					case "select":
+						var idObj=splitTopic[3];
+						console.log("Select");
+						console.log("ID : "+idObj);
+						selectObject(idObj,lastSelect);
+						break;
+				}
 			}
 }
